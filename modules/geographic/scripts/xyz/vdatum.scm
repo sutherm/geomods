86a18c734fda57d5d3f1322adb0c13dbbf13a8d6
Matthew Love <matthew.love@colorado.edu>
2018-09-26 19:46:21 -0700

    updates to vdatum

--- modules/geographic/scripts/xyz/vdatum.scm
+++ modules/geographic/scripts/xyz/vdatum.scm
@@ -15,18 +15,18 @@
 ;;
 ;;; Commentary:
 ;;
-;; usage: vdatum [ -ghiorvzFGLMV [ args ] ] [ file ]
+;; usage: vdatum [ -fghiorvz [ args ] ] [ file ]
 ;;
 ;; Run vdatum from command-line.
 ;;
 ;; Use -g to run the VDatum GUI
 ;;
-;; Exampe:
-;;  vdatum input.xyz -F --ivert mhw --overt navd88
+;; Example:
+;;  vdatum input.xyz -f --ivert mhw --overt navd88
 ;;
 ;;; Code:
 
-(define-module (geographic scripts dem vdatum)
+(define-module (geographic scripts xyz vdatum)
   #:use-module (ice-9 getopt-long)
   #:use-module (ice-9 format)
   #:use-module (ice-9 rdelim)
@@ -39,31 +39,31 @@
 
 (define dem-config 
   (cond 
-   ((file-exists? (string-append (getenv "HOME") "/.dem.conf"))
-    (string-append (getenv "HOME") "/.dem.conf"))
-   ((file-exists? (string-append (getenv "HOME") "/.dem.scm"))
-    (string-append (getenv "HOME") "/.dem.scm"))
+   ((file-exists? (string-append (getenv "HOME") "/.xyz.conf"))
+    (string-append (getenv "HOME") "/.xyz.conf"))
+   ((file-exists? (string-append (getenv "HOME") "/.xyz.scm"))
+    (string-append (getenv "HOME") "/.xyz.scm"))
    (else
-    (string-append (getenv "HOME") "/.dem"))))
+    (string-append (getenv "HOME") "/.xyz"))))
 
 (if (file-exists? dem-config)
     (load dem-config))
 
-(define vdatum-version "0.0.2")
+(define vdatum-version "0.0.3")
 
 (define %summary "Run NOAA's VDatum from command-line.")
 
 (define command-synopsis
   '((version (single-char #\v) (value #f))O))
 
-(define %include-in-dem-list #t)
+(define %include-in-xyz-list #t)
 
 (define (display-help)
   (format #t "\
 ~a
 vdatum
 
-usage: vdatum [ -ghiorvzFGLMV [ args ] ] [ file ]
+usage: vdatum [ -fghiorvz [ args ] ] [ file ]
 " %summary))
 
 (define (display-version)
@@ -77,7 +77,7 @@ There is NO WARRANTY, to the extent permitted by law.
 " vdatum-version))
 
 (define (find-vdatum)
-  (format (current-error-port) "dem: Attempting to locate vdatum.~%")
+  (format (current-error-port) "xyz: Attempting to locate vdatum.~%")
   (let ((fdi (open-input-pipe "find / -type f 2> /dev/null | grep 'vdatum\\.jar'")))
     (read-line fdi)))
 
@@ -85,23 +85,17 @@ There is NO WARRANTY, to the extent permitted by law.
   '((version (single-char #\v) (value #f))
     (help (single-char #\h) (value #f))
     (gui (single-char #\g) (value #f))
-    (file (single-char #\F) (value #f))
+    (file (single-char #\f) (value #f))
     (ivert (single-char #\i) (value #t))
     (overt (single-char #\o) (value #t))
     (ihorz (single-char #\r) (value #t))
-    (ohorz (single-char #\z) (value #t))
-    (lidar (single-char #\L) (value #f))
-    (gdal (single-char #\G) (value #f))
-    (mbio (single-char #\M) (value #f))))
+    (ohorz (single-char #\z) (value #t))))
 
 (define (vdatum . args)
   (let* ((options (getopt-long (cons "vdatum" args) command-synopsis)))
     (let ((help-wanted (option-ref options 'help #f))
 	  (version-wanted (option-ref options 'version #f))
 	  (gui (option-ref options 'gui #f))
-	  (lidar (option-ref options 'lidar #f))
-	  (gdal (option-ref options 'gdal #f))
-	  (mbio (option-ref options 'mbio #f))
 	  (want-file (option-ref options 'file #f))
 	  (ivert (option-ref options 'ivert "mllw:m:height"))
 	  (overt (option-ref options 'overt "navd88:m:height"))
@@ -123,22 +117,22 @@ There is NO WARRANTY, to the extent permitted by law.
 		   (if gui
 		       (system (string-append "java -jar " vdatum-path))
 		       (if (pair? input)
-			   (cond 
-			    (want-file
-			     (vdatum-file (car input)  #:vdatum vdatum-path #:ihorz ihorz #:ohorz ohorz #:ivert ivert #:overt overt))
-			    (lidar
-			     (port->vdatum (las->port (car input)) #:vdatum vdatum-path #:ihorz ihorz #:ohorz ohorz #:ivert ivert #:overt overt))
-			    (gdal
-			     (port->vdatum (gdal->port (car input)) #:vdatum vdatum-path #:ihorz ihorz #:ohorz ohorz #:ivert ivert #:overt overt))
-			    (mbio
-			     (port->vdatum (mbio->port (car input)) #:vdatum vdatum-path #:ihorz ihorz #:ohorz ohorz #:ivert ivert #:overt overt))
-			    (else
-			     (port->vdatum infile #:vdatum vdatum-path #:ihorz ihorz #:ohorz ohorz #:ivert ivert #:overt overt)))
-
+			   (if want-file
+			       (vdatum-file (car input)  #:vdatum vdatum-path #:ihorz ihorz #:ohorz ohorz #:ivert ivert #:overt overt)
+			       (port->vdatum infile #:vdatum vdatum-path #:ihorz ihorz #:ohorz ohorz #:ivert ivert #:overt overt))
 			   (port->vdatum infile #:vdatum vdatum-path #:ihorz ihorz #:ohorz ohorz #:ivert ivert #:overt overt)))))))))))
+			   ;;  (lidar
+			   ;;   (port->vdatum (las->port (car input)) #:vdatum vdatum-path #:ihorz ihorz #:ohorz ohorz #:ivert ivert #:overt overt))
+			   ;;  (gdal
+			   ;;   (port->vdatum (gdal->port (car input)) #:vdatum vdatum-path #:ihorz ihorz #:ohorz ohorz #:ivert ivert #:overt overt))
+			   ;;  (mbio
+			   ;;   (port->vdatum (mbio->port (car input)) #:vdatum vdatum-path #:ihorz ihorz #:ohorz ohorz #:ivert ivert #:overt overt))
+			   ;;  (else
+			   ;;   (port->vdatum infile #:vdatum vdatum-path #:ihorz ihorz #:ohorz ohorz #:ivert ivert #:overt overt)))
+		       
+		       ;; (port->vdatum infile #:vdatum vdatum-path #:ihorz ihorz #:ohorz ohorz #:ivert ivert #:overt overt)))))))))))
 
 	    
 (define main vdatum)
 
 ;;; End
-;;(system (string-append "java -jar " vdatum-path " " (string-join args " ")))))
